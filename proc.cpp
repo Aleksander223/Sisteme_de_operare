4b844f76fb4ed0454962fc8fcf4e71c16524bd93
iustin <iustin@privacysystems.eu>
2020-01-09 11:10:38 +0200

    .

--- proc.cpp
+++ proc.cpp
@@ -21,7 +21,7 @@ namespace proc {
     private:
         const std::string path = "/proc";
 
-        std::vector<int> procList;
+
         std::map<int, std::vector<int>> parentDict;// un PPid -> vector de Pid asociati lui
 
         int getParentID (int pid) {
@@ -34,12 +34,10 @@ namespace proc {
             std::ifstream fin(statPath);
             std::string line;
             for(unsigned i=0;i<7;i++)
-                std::getline(fin,line);   
+                std::getline(fin,line);
             fin.close();
             std::stringstream conv(line.substr(line.find("\t"))); //convert string to int
             conv>>ppid;
-            // std::cout<<ppid<<"\n";
-            // std::ifstream stat(statPath);
 
             return ppid;
         }
@@ -50,26 +48,20 @@ namespace proc {
                 if (fileName[0] >= '0' && fileName[0] <= '9') {
                     int processID = std::stoi(fileName);
 
-                    procList.push_back(processID);
                     parentDict[getParentID(processID)].push_back(processID);
                 }
             }
         }
 
         void printProcesses() {
-            // Procese:
-            // for (auto i: procList) {
-            //     std::cout << i << std::endl;
-            // }
             // Procese grupate dupa PPid:
              for(auto it = parentDict.begin();it != parentDict.end();it++)
-              {  
+              {
                 std::cout << "\033[1;31m"<<(it->first)<<"\t->"<<"\033[0m";
                     for(auto it2 : it->second)
                         std::cout<<it2<<" ";
                 std::cout<<"\n";
                 }
-  
         }
 
         ProcessScraper() {
