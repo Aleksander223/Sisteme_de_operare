7b20978603462e29276133d5c949d226f998711d
PnzJust <panzaru.iustin@yahoo.com>
2020-01-14 12:29:58 +0200

    Structura directoarelor arborescenta

--- proc.cpp
+++ proc.cpp
@@ -19,10 +19,10 @@ namespace proc {
         const std::string path = "/proc";
 
 
-        std::map<int, std::vector<int>> parentDict;// un PPid -> vector de Pid asociati lui
+        std::map<std::string, std::vector<int>> parentDict;// un PPid -> vector de Pid asociati lui
 
-        int getParentID (int pid) {
-            int ppid; // PPid se afla in /proc/pid/status la randul ce incepe cu PPid -> linia 7
+        std::string getParentID (int pid) {
+            std::string ppid; // PPid se afla in /proc/pid/status la randul ce incepe cu PPid -> linia 7
 
             // status file
             std::string statPath = "";
@@ -33,9 +33,7 @@ namespace proc {
             for(unsigned i=0;i<7;i++)
                 std::getline(fin,line);
             fin.close();
-            std::stringstream conv(line.substr(line.find("\t"))); //convert string to int
-            conv>>ppid;
-
+            ppid = line.substr(line.find("\t")+1);
             return ppid;
         }
     public:
@@ -52,13 +50,34 @@ namespace proc {
 
         void printProcesses() {
             // Procese grupate dupa PPid:
-             for(auto it = parentDict.begin();it != parentDict.end();it++)
-              {
-                std::cout << "\033[1;31m"<<(it->first)<<"\t->"<<"\033[0m";
-                    for(auto it2 : it->second)
-                        std::cout<<it2<<" ";
-                std::cout<<"\n";
+            // for(auto it = parentDict.begin();it != parentDict.end();it++)
+            //  {
+            //    std::cout << "\033[1;31m"<<(it->first)<<"\t->"<<"\033[0m";        // afisare doar pt verificare
+            //        for(auto it2 : it->second)
+            //            std::cout<<it2<<" ";
+            //    std::cout<<"\n";
+            //    }
+            std::experimental::filesystem::remove_all("./bin/0/");          // stergem tot ce era inainte
+            std::experimental::filesystem::create_directory("./bin/0/");    // creem radacina
+            createPSTree("0","./bin/0/");                                   // recursiv toti fii, nepotii etc
+
+        }
+
+        void createPSTree(std::string ppid, std::string current_path) {
+
+            if(parentDict.find(ppid) != parentDict.end())
+                for (auto it: parentDict[ppid])
+                {
+                    std::string new_path = current_path  + std::to_string(it) + "/";
+                    std::experimental::filesystem::create_directory(new_path);
+                    createPSTree(std::to_string(it), new_path);
                 }
+            else
+            {
+                // nu il gaseste deci e frunza
+                // daca e frunza trebuie sa scriem in el un fisier status 
+            }
+
         }
 
         ProcessScraper() {
