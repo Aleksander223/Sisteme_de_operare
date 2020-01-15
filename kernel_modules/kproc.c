f7614bfd717b54d8471d82c6cc29f41b7a6e9719
Aleks <alexandruvlad.adam@gmail.com>
2020-01-15 11:43:54 +0200

    Added character device

--- kernel_modules/kproc.c
+++ kernel_modules/kproc.c
@@ -5,14 +5,65 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+
+#define DEVICE_NAME "kproc"
+
+static int dev_open(struct inode*, struct file*);
+static int dev_release(struct inode*, struct file*);
+static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
+static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+
+static struct file_operations fops = {
+    .open = dev_open,
+    .read = dev_read,
+    .write = dev_write,
+    .release = dev_release,
+};
+
+static int major;
 
 static int __init kproc_init(void) {
-    printk("Test\n");
+    major = register_chrdev(0, DEVICE_NAME, &fops);
+
+    if (major < 0) {
+        printk(KERN_ALERT "Kproc load failed\n");
+        return major;
+    }
+
+    printk("Init Kproc... %d\n", major);
     return 0;
 }
 
 static void __exit kproc_exit(void) {
-    printk("Exit test\n");
+    printk("EXit Kproc..\n");
+}
+
+
+static int dev_open(struct inode *inodep, struct file *filep) {
+    printk(KERN_INFO "Kproc opened\n");
+    return 0;
+}
+
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
+    printk(KERN_INFO "KProc is read-only\n");
+    return -EFAULT;
+}
+
+static int dev_release(struct inode *inodep, struct file *filep) {
+    printk(KERN_INFO "Kproc closed\n");
+    return 0;
+}
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
+    int errors = 0;
+    char *message = "Test";
+    int message_len = strlen(message);
+
+    errors = copy_to_user(buffer, message, message_len);
+
+    return errors == 0 ? message_len : -EFAULT;
 }
 
 module_init(kproc_init);
