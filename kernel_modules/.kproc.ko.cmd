0f0d71bc0fe284924868d4ee6564f7fbdf510c4f
Aleks <alexandruvlad.adam@gmail.com>
2020-01-16 17:23:57 +0200

    Removed binaries

--- kernel_modules/.kproc.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/stefan/Desktop/Sisteme_de_operare/kernel_modules/kproc.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/stefan/Desktop/Sisteme_de_operare/kernel_modules/kproc.ko /home/stefan/Desktop/Sisteme_de_operare/kernel_modules/kproc.o /home/stefan/Desktop/Sisteme_de_operare/kernel_modules/kproc.mod.o ;  true
--- kernel_modules/.tmp_versions/kproc.mod
+++
@@ -1,3 +0,0 @@
-/home/stefan/Desktop/Sisteme_de_operare/kernel_modules/kproc.ko
-/home/stefan/Desktop/Sisteme_de_operare/kernel_modules/kproc.o
-
