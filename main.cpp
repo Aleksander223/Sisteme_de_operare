69aa33a93287a34bd68b5c4d469911fe40c4d46f
Cosmin2108 <cosmin.stefan952@yahoo.com>
2020-01-16 23:07:23 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-//#include "fs.cpp"
-#include "proc.cpp"
+#include "fs.cpp"
+
 int main(int argc, char* argv[]) {
+    proc::ps.printProcesses();
     return 0;
 
     // return fuse_main( argc, argv,&operations, NULL);
-
 }
